ba9c6fdcba28972bf9c023a4af51a3cacc96b918
SilonCrutch <cfielda@gmail.com>
2022-11-14 18:22:54 +0800

    new classes

--- Player.py
+++ Player.py
@@ -9,3 +9,17 @@ class Player:
     # we want all players to get their next move given a game
     def get_move(self, game):
         pass
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
