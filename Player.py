6773b492e329d6c24fdd0cecdb31abde4f1e780b
SilonCrutch <cfielda@gmail.com>
2022-11-14 18:16:00 +0800

    added class

--- Player.py
+++ Player.py
@@ -1,3 +1,11 @@
 import math
 import random
 
+class Player:
+    def __init__(self, letter):
+        # letter is x or o
+        self.letter = letter
+
+    # we want all players to get their next move given a game
+    def get_move(self, game):
+        pass
