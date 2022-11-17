48bf9d936b7bf2119efa154d2a4cfadf430b57ef
SilonCrutch <cfielda@gmail.com>
2022-11-17 12:24:59 +0800

    added new statements

--- Game.py
+++ Game.py
@@ -23,8 +23,25 @@ class TicTacToe:
         #    if spot == ' ':
         #        moves.append(i)
         #return moves
-    
+
+    def empty_squares(self):
+        return ' ' in self.board
+
+    def num_empty_squares(self):
+        return self.board.count(' ')
+
+    def make_move(self, square, letter):
+        # if valid move, then make the move (assign square to letter)
+        # then return true, if invalid, return false
+        if self.board[square] == ' ':
+            self.board[square] = letter
+            if self.winner(square, letter):
+                self.current_winner = letter
+            return True
+        return False
+
 def play(game, x_player, o_player, print_game=True):
+    # returns the winner of the game or None for a tie
     if print_game:
         game.print_board_nums()
 
@@ -33,3 +50,25 @@ def play(game, x_player, o_player, print_game=True):
     # (we don't have to worry about winner because we'll just return that)
     # which breaks the loop)
     while game.empty_squares():
+        # get the move from the appropriate player
+        if letter == 'O':
+            square = o_player.get_move(game)
+        else: 
+            square = x_player.get_move(game)
+
+        # let's define a function to make a move!
+        if game.make_move(square, letter):
+            if print_game:
+                print(letter,f'makes a move square {square}')
+                game.print_board()
+                print('') #just empty line
+            
+            if game.current_winner:
+                if print_game:
+                    print(letter,'wins!')
+                return letter
+
+            letter = "O" if letter == "X" else "X"
+        
+        if print_game:
+            print("It's a tie!")
