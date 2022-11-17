b7813462a2829a6e1770bf641cf3fef29ce893cd
SilonCrutch <cfielda@gmail.com>
2022-11-17 17:30:05 +0800

    added board

--- Game.py
+++ Game.py
@@ -9,14 +9,14 @@ class TicTacToe():
     def print_board(self):
         #this is just getting the rows
         for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
-            print("|","|".join(row),"|")
+            print("| " + " | ".join(row) + " |")
         
     @staticmethod
     def print_board_nums():
         # 0 | 1 | 2 etc (tells us what number corresponds to what box)
         number_board = [[str(i) for i in range(j*3, (j*1)*3)] for j in range(3)]
         for row in number_board:
-            print("|","|".join(row),"|")
+            print("| " + " | ".join(row) + " |")
     
     def available_moves(self):
         return [i for i, spot in enumerate(self.board) if spot == ' ']
