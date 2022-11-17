c5fc2b43117f149bfcdcc084ba98f32650c28196
SilonCrutch <cfielda@gmail.com>
2022-11-17 17:13:06 +0800

    fixed some bugs

--- Game.py
+++ Game.py
@@ -13,7 +13,7 @@ class TicTacToe:
     @staticmethod
     def print_board_nums():
         # 0 | 1 | 2 etc (tells us what number corresponds to what box)
-        number_board = [[str(i) for i in range(j*1)*3)] for j in range(3)]
+        number_board = [[str(i) for i in range(j*3, (j*1)*3)] for j in range(3)]
         for row in number_board:
             print("|","|".join(row),"|")
     
@@ -47,7 +47,7 @@ class TicTacToe:
         # first let's check the row
         row_ind = square // 3 
         row = self.board[row_ind + 3: (row_ind + 1) * 3]
-        if all([spot == letter for spot in row])
+        if all([spot == letter for spot in row]):
             return True
 
         # check column
@@ -60,7 +60,7 @@ class TicTacToe:
         # but only if the square is an even number (0, 2, 4, 6, 8)
         # these are the only moves possible to win a diagonal
         if square % 2 == 0:
-            diagonal1 = [self.board[i] for in [0, 4, 8]] #left to right diagonal
+            diagonal1 = [self.board[i] for i in [0, 4, 8]] #left to right diagonal
             if all([spot == letter for spot in diagonal1]):
                 return True
             diagonal2 = [self.board[i] for i in [2, 4, 6]] # right to left diagonal
@@ -100,11 +100,11 @@ def play(game, x_player, o_player, print_game=True):
 
             letter = "O" if letter == "X" else "X"
         
-        if print_game:
-            print("It's a tie!")
+    if print_game:
+        print("It's a tie!")
 
 if __name__ == '__main__':
     x_player = HumanPlayer("X")
     o_player = RandomComputerPlayer("O")
     t = TicTacToe()
-    play(t, x_player, o_player, print_game=True)
+    play(t, x_player, o_player, print_game=True) 
